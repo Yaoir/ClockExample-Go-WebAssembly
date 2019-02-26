6c359f6c452760a16a94adb1d69be37810d949f0
Jay Ts <jay@jayts.com>
2019-02-25 19:42:14 -0700

    Updated to Go 1.12 FuncOf()

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import "syscall/js"
 // Callback for the interval timer
 // Get the current time and update it in the DOM
 
-func update_time(args []js.Value) {
+func update_time(this js.Value, args []js.Value) interface{} {
 	// Get the current date in this locale
 	// It's done like this in JavaScript:
 	//	date = new Date()
@@ -22,25 +22,23 @@ func update_time(args []js.Value) {
 	//	document.getElementById("clock").textContent = s
 
 	js.Global().Get("document").Call("getElementById", "clock").Set("textContent", s)
+	return nil
 }
 
 func main() {
-	// channel for this goroutine
-	c := make(chan struct{}, 0)
-
 	// Set up a recurring timer event to call update_time() every 200 ms.
 	// It's done like this in JavaScript:
 	//	setInterval(update_time,200)
 
 	// Create JavaScript callback connected to update_time()
 
-	timer_cb := js.NewCallback(update_time)
+	timer_cb := js.FuncOf(update_time)
 
 	// Set timer to call timer_cb() every 200 ms.
 
 	js.Global().Call("setInterval",timer_cb,"200")
 
-	// Keep this goroutine from exiting by waiting on the channel.
-	// Nothing is ever sent on it, so this will block.
-	<-c
+	// An empty select blocks, so the main() function will never exit.
+	// This allows the event handler callbacks to continue operating.
+	select{}
 }
